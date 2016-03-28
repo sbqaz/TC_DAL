cfd8ac3539fcf87abf55b132a43c22e8dbd2b1b8
Ao Li <201407737@post.au.dk>
2016-03-28 07:08:11 +0200

    login lib function??

--- TrafficControl.DAL/TCApi.cs
+++ TrafficControl.DAL/TCApi.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using TrafficControl.DAL.Types;
 
 namespace TrafficControl.DAL
 {
@@ -9,13 +12,33 @@ namespace TrafficControl.DAL
     {
         public TCApi() { }
 
-        public bool LogIn(string email, string encryptedPassWord)
+        public bool LogIn(string email, string PassWord)
         {
+
             //Change to some API call
-            if (email == "1234" && encryptedPassWord == "1234")
-                return true;
-            else
-                return false;
+            using (var client = new HttpClient())
+            {
+                //ask bork what our url is  
+                client.BaseAddress = new Uri("http://localhost:9000/");
+                //client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                User usr = new User() {email = email, pass = PassWord};
+                var resp = client.PostAsJsonAsync("api/Login",usr).Result;
+                if (resp.IsSuccessStatusCode == true)
+                    return true;
+                else
+                    return false; 
+                        
+                var response = client.GetAsync("api/Login").Result;
+                response.EnsureSuccessStatusCode();
+                
+
+    }
+
+            //if (email == "1234" && encryptedPassWord == "1234")
+            //    return true;
+            //else
+            //    return false;
         }
     }
 }
