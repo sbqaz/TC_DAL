d6dce62a2cf72c5f96a7eb5a411433ae672dba55
Ao Li <201407737@post.au.dk>
2016-04-14 12:54:22 +0200

    interface change inc
    interface change inc

--- TrafficControl.DAL.RestSharp/ITCApi.cs
+++ TrafficControl.DAL.RestSharp/ITCApi.cs
@@ -1,11 +1,14 @@
-﻿namespace TrafficControl.DAL.RestSharp
+﻿using System.Collections.Generic;
+using TrafficControl.DAL.RestSharp.Types;
+namespace TrafficControl.DAL.RestSharp
 {
     public interface ITCApi
     {
         bool LogIn(string email, string encryptedPassWord);
-        bool CreateUser(string email, string passWord, string name, int privileges);
-        bool UpdateUser(string email, string passWord, string name, int privileges, int id);
+        bool CreateUser(string email, string passWord, string name, string privileges);
+        bool UpdateUser(string email, string passWord, string name, string privileges, int id);
         bool deleteUser(int id);
         bool ChangePassword(string Opassword, string NPassword);
+        ICollection<Case> GetCases();
     }
 }
