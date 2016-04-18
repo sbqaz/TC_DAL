202f3c21e1f9d9f0022d0e3ed02971aa4059e288
Ao Li <201407737@post.au.dk>
2016-04-18 11:01:07 +0200

    changes

--- TrafficControl.DAL.RestSharp/Types/User.cs
+++ TrafficControl.DAL.RestSharp/Types/User.cs
@@ -1,17 +1,24 @@
 using System;
+using Newtonsoft.Json;
 
 namespace TrafficControl.DAL.RestSharp.Types
 {
     public partial class User
     {
+        [JsonProperty("Id")]
         public string Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        [JsonProperty("FirstName")]
         public string FirstName { get; set; }
+        [JsonProperty("LastName")]
         public string LastName { get; set; }
         public int Privileges { get; set; }
+        [JsonProperty("PhoneNumber")]
         public string Number { get; set; }
+        [JsonProperty("EmailNotification")]
         public bool EmailNotification { get; set; }
+        [JsonProperty("SMSNotification")]
         public bool SMSNotification { get; set; }
     }
 }
