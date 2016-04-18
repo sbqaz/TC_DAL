8017e2fbf0047a6e175ec130c9fc2a44c1a0e535
Ao Li <201407737@post.au.dk>
2016-04-18 11:06:22 +0200

    remove some propety from user

--- TrafficControl.DAL.RestSharp/Types/User.cs
+++ TrafficControl.DAL.RestSharp/Types/User.cs
@@ -7,18 +7,20 @@ namespace TrafficControl.DAL.RestSharp.Types
     {
         [JsonProperty("Id")]
         public string Id { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
         [JsonProperty("FirstName")]
         public string FirstName { get; set; }
         [JsonProperty("LastName")]
         public string LastName { get; set; }
-        public int Privileges { get; set; }
+        
         [JsonProperty("PhoneNumber")]
         public string Number { get; set; }
         [JsonProperty("EmailNotification")]
         public bool EmailNotification { get; set; }
         [JsonProperty("SMSNotification")]
         public bool SMSNotification { get; set; }
+
+        //public string Username { get; set; }
+        //public string Password { get; set; }
+        //public int Privileges { get; set; }
     }
 }
