0849fb0e3de3a2ce2de4c57e855d497b8d028c36
Ao Li <201407737@post.au.dk>
2016-05-05 12:20:54 +0200

    may god help me with unitesting

--- ClassLibrary1/TCDataConnection.Test.Unit.cs
+++ ClassLibrary1/TCDataConnection.Test.Unit.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using TrafficControl.DAL.RestSharp;
 
 namespace DAL.Test.Unit
 {
+    
+    [TestFixture]
     public class TCDataConnectionTests
     {
+        private TCDataConnection uut { get; set; }
+
+        [SetUp]
+        public void Init()
+        {
+            uut = new TCDataConnection();
+        }
     }
 }
