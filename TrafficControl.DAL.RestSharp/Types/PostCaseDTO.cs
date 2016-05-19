6c052d1dadbeae5bf45706ec6b0ad6164b905ed8
jacobboes <jacobboeseriksen@hotmail.com>
2016-05-19 20:37:18 +0200

    Added JsonProperty InstallationId

--- TrafficControl.DAL.RestSharp/Types/PostCaseDTO.cs
+++ TrafficControl.DAL.RestSharp/Types/PostCaseDTO.cs
@@ -1,8 +1,11 @@
+using Newtonsoft.Json;
+
 namespace TrafficControl.DAL.RestSharp
 {
     public class PostCaseDTO 
     {
         public string ErrorDescription { get; set; }
+        [JsonProperty("InstallationId")]
         public long Installation { get; set; }
         public ObserverSelection Observer { get; set; }
     }
